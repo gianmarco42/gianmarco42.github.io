56e3c47569f90d1206fb72fbb1d64f238518cfc9
gianmarco42 <gianmarco.varela@jpschools.org>
2026-02-12 17:04:47 +0000

    dice

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
   // Your code goes here
-
+  $("<div>")
+    .css("height", 15)
+    .css("width", 15)
+    .css("background-color", "black")
+    .css("position", "absolute")
+    .css("top", 50)
+    .css("left", 50)
+    .appendTo("#die");
 });
+
+function rollDie(dieId) {
+  console.log("clicked");
+}
+
+function handleClick() {
+  rollDie("#die");
+}
+
+$("#die").on("click", handleClick);
