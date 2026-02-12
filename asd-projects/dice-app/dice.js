b91cd6c8694d7e35d3a3c7804026e6b35a744381
gianmarco42 <gianmarco.varela@jpschools.org>
2026-02-12 17:24:13 +0000

    todo 8

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -9,8 +9,19 @@ $(document).ready(function () {
     .css("left", 50)
     .appendTo("#die");
     function makeDot(top, left, elementId) {
-
+$("<div>")
+    .css("height", 15)
+    .css("width", 15)
+    .css("background-color", "black")
+    .css("position", "absolute")
+    .css("top", top)
+    .css("left", left)
+    .css("border-radius", "50%")
+    .appendTo(elementID);
     }
+makeDot(50, 50, "#die"); // middle middle
+makeDot(25, 25, "#die"); // top left
+makeDot(75, 75, "#die"); // bottom right
 function rollDie(dieId) {
   console.log("clicked");
   var randomNum = Math.ceil(Math.random() * 6);
